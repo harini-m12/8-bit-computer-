1d64158059dd85ea2228039b9dcd3085915d6b5c
harini-m12 <harinimadhavaraj12@gmail.com>
2026-06-20 09:01:28 +0530

    Update isa.v

--- rtl/isa.v
+++ rtl/isa.v
@@ -1,10 +1,16 @@
-// isa.vh
-
+// isa.v
 `define NOP 4'b0000
 `define ADD 4'b0001
 `define SUB 4'b0010
 `define LDA 4'b0011
 `define LDB 4'b0100
 `define OUT 4'b0101
-`define JMP 4'b0110
+// JMP is 0b1011, JC=1100, JZ=1101, INC=1110
+`define STA 4'b1000
+`define XOR 4'b1001
+`define AND 4'b1010
+`define JMP 4'b1011
+`define JC  4'b1100
+`define JZ  4'b1101
+`define INC 4'b1110
 `define HLT 4'b0111
